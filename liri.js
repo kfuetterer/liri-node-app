f527252cd464415d42ad3ad711ac8d2372591364
Katharina Fuetterer <Katharina Fuetterer>
2017-04-05 16:25:51 -0400

    Update 04-05-2017 425

--- liri.js
+++ liri.js
@@ -3,6 +3,7 @@ var spotify = require('spotify');
 var request = require('request');
 var authorizationReference = require("./keys.js");
 var authorization = authorizationReference.twitterKeys;
+var filesystem = require("fs");
 
 var consumer_key = authorization.consumer_key;
 var consumer_secret = authorization.consumer_secret;
@@ -16,25 +17,48 @@ var client = new Twitter({
   access_token_secret: access_token_secret
 });
 
-if (process.argv[2] === "my-tweets") {
+var arguments = process.argv;
+var action = process.argv[2];
+
+switch (action) {
+  case "my-tweets":
+    myTweets();
+    break;
+
+  case "spotify-this-song":
+    spotifySong();
+    break;
+
+  case "movie-this":
+    movieThis();
+    break;
+}
+
+function myTweets(params) {
 
     var params = {screen_name: 'KatFuetterer'};
     client.get('statuses/user_timeline', params, function(error, tweets, response) {
         if (!error) {
             for (var i = 0; i < tweets.length; i++) {
                 console.log("TWEET " + i + ": " + tweets[i].text);
+                filesystem.appendFile("log.txt", "\n" + tweets[i].text, function(err) {
+                    if (err) {
+                        return console.log(err);
+                    }
+                    console.log("log.txt updated!");
+                });
             }
         }
     });
 }
 
-if (process.argv[2] === "spotify-this-song") {
+function spotifySong(song) {
 
-    var arguments = process.argv;
-    var track = [];
-
-    for (var i = 3; i < arguments.length; i++) {
-        track.push(arguments[i]);
+    var track = [song];
+    if (typeof arguments !== 'undefined') {
+        for (var i = 3; i < arguments.length; i++) {
+            track.push(arguments[i]);
+        }
     }
 
     spotify.search({ type: 'track', query: track }, function(err, data) {
@@ -47,17 +71,21 @@ if (process.argv[2] === "spotify-this-song") {
             console.log("TITLE: " + data.tracks.items[0].name);
             console.log("ALBUM NAME: " + data.tracks.items[0].album.name);
             console.log("SPOTIFY URL" + data.tracks.items[0].preview_url);
+            filesystem.appendFile("log.txt", "\n" + data.tracks.items[0].artists[0].name, function(err) {});
+            filesystem.appendFile("log.txt", "\n" + data.tracks.items[0].name, function(err) {});
+            filesystem.appendFile("log.txt", "\n" + data.tracks.items[0].album.name, function(err) {});
+            filesystem.appendFile("log.txt", "\n" + data.tracks.items[0].preview_url, function(err) {});
         }
     });
 }
 
-if (process.argv[2] === "movie-this") {
+function movieThis(movieRandom) {
 
-    var arguments = process.argv;
-    var movie = [];
-
-    for (var i = 3; i < arguments.length; i++) {
-        movie.push(arguments[i]);
+    var movie = [movieRandom];
+    if(typeof arguments !== 'undefined') {
+        for (var i = 3; i < arguments.length; i++) {
+            movie.push(arguments[i]);
+        }
     }
 
     request("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&r=json", function (error, response, body) {
@@ -71,10 +99,28 @@ if (process.argv[2] === "movie-this") {
             console.log("ACTORS: " + JSON.parse(body).Actors);
             console.log("ROTTEN TOMATOES RATING: " + JSON.parse(body).Ratings[1].Value);
             console.log("ROTTEN TOMATOES URL: " + JSON.parse(body).Ratings[1].Source);
+            filesystem.appendFile("log.txt", "\n" + JSON.parse(body).Title, function(err) {});
+            filesystem.appendFile("log.txt", "\n" + JSON.parse(body).Released, function(err) {});
+            filesystem.appendFile("log.txt", "\n" + JSON.parse(body).imdbRating, function(err) {});
+            filesystem.appendFile("log.txt", "\n" + JSON.parse(body).Country, function(err) {});
+            filesystem.appendFile("log.txt", "\n" + JSON.parse(body).Language, function(err) {});
+            filesystem.appendFile("log.txt", "\n" + JSON.parse(body).Plot, function(err) {});
+            filesystem.appendFile("log.txt", "\n" + JSON.parse(body).Actors, function(err) {});
+            filesystem.appendFile("log.txt", "\n" + JSON.parse(body).Ratings[1].Value, function(err) {});
+            filesystem.appendFile("log.txt", "\n" + JSON.parse(body).Ratings[1].Source, function(err) {});
         }
     })
 }
 
 if (process.argv[2] === "do-what-it-says") {
+    filesystem.readFile("random.txt", "utf8", function(error, data) {
+        var dataArr = data.split(",");
 
+        if(dataArr[0] === "spotify-this-song") {
+            spotifySong(dataArr[1]);
+        }
+        else if(dataArr[0] === "movie-this") {
+            movieThis(dataArr[1]);
+        }
+    })
 }
